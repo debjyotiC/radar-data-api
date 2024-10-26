7c94e450ef88cb529345a4a2170f884d3125a033
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2024-10-26 21:20:01 +0530

    values bug

--- test-detected-object.py
+++ test-detected-object.py
@@ -19,7 +19,7 @@ plt.figure(figsize=(6, 6))
 # Continuously read radar data
 try:
     while True:
-        range_doppler, range_profile, detected_objects, frameOk = radar_data_reader.read_radar_data(data_port, cf)
+        range_doppler, range_profile, detected_objects, range_azimuth, frameOk = radar_data_reader.read_radar_data(data_port, cf)
 
         if frameOk:
 
